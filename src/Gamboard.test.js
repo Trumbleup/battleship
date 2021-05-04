982ff36b11b069a9751c91ef6d63c875d3ca0854
Danny <Dtrumble12@gmail.com>
2021-05-03 20:55:12 -0500

    Added a test to check if the isSunk ship method properly works in context of the gameboard

--- src/Gamboard.test.js
+++ src/Gamboard.test.js
@@ -100,6 +100,36 @@ describe("Testing receive attack functions", () => {
     })
 })
 
+describe("Testing the sinking functionality", () => {
+    let gameBoard1;
+    let ship1;
+    let ship2;
+    let ship3;
+    let ship4;
+    let ship5;
+    beforeEach(() => {
+        gameBoard1 = Gameboard();
+        ship1 = Ship(5, 'carrier');
+        ship2 = Ship(4, 'battleship');
+        ship3 = Ship(3, 'cruiser');
+        ship4 = Ship(3, 'submarine');
+        ship5 = Ship(2, 'destroyer');
+        gameBoard1.placeShip(ship1, ["B1", "B2", "B3", "B4", "B5"]);
+        gameBoard1.placeShip(ship2, ["D1", "E1", "F1", "G1"]);
+        gameBoard1.placeShip(ship3, ["D3", "D4", "D5"]);
+        gameBoard1.placeShip(ship4, ["E6", "E7", "E8"]);
+        gameBoard1.placeShip(ship5, ["F5", "G5"]);   
+    })
+    test('Carrier isSunk should equal true', () => {
+        gameBoard1.receiveAttack("B1");
+        gameBoard1.receiveAttack("B2");
+        gameBoard1.receiveAttack("B3");
+        gameBoard1.receiveAttack("B4");
+        gameBoard1.receiveAttack("B5");
+        expect(gameBoard1.shipPlacements['carrier'].ship.isSunk).toBeTruthy();
+    })
+})
+
 
 
 
