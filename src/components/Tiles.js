b7cd2f7f1db34e35f62c112291bd02eb6be4f920
Danny <Dtrumble12@gmail.com>
2021-05-07 17:59:49 -0500

    Added coordinate attributes to each tile

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -1,15 +1,25 @@
 import './GameboardDisplay.css'
 
-const Tile = ({ refWidth, refHeight }) => {
+const Tile = ({ refWidth, refHeight, coordinate }) => {
     return (
-        <div style={{width: (1/10) * refWidth, height: (1/10) * refHeight}} className="black-border border-box"></div>
+        <div 
+        style={{width: (1/10) * refWidth, height: (1/10) * refHeight}}
+        className="black-border border-box"
+        data-coordinate={coordinate}
+        >    
+        </div>
     )
 }
 
 const Tiles = ({ refWidth, refHeight }) => {
-    const tilesArr = []
-    for (let x = 1; x <= 100; x++) {
-        tilesArr.push(<Tile refWidth={refWidth} refHeight={refHeight}/>);
+    const alphabetRange = "ABCDEFGHIJ".split('');
+    const tilesArr = [];
+    for (let x = 0; x < 10; x++) {
+        let coordinateLetter = alphabetRange[x];
+        for (let y = 1; y <= 10; y++) {
+            let coordinate = coordinateLetter + y;
+            tilesArr.push(<Tile refWidth={refWidth} refHeight={refHeight} key={coordinate} coordinate={coordinate}/>);
+        }   
     }
     return (
         <div className="flex-row">
