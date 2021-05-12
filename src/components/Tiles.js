e91f953259af7e22ccf8f45be199a23210316575
Danny <Dtrumble12@gmail.com>
2021-05-12 15:25:45 -0500

    Created a separate file for the Tile component

--- src/components/Tiles.js
+++ src/components/Tiles.js
@@ -1,45 +1,8 @@
-import { useEffect, useState } from 'react'
+import Tile from './Tile'
 import './GameboardDisplay.css'
 
 
-const Tile = ({ refWidth, refHeight, coordinate, shipPlacements }) => {
-    const [isShot, setIsShot] = useState(false);
-    const [hasShip, setHasShip] = useState(false);
 
-    const handleIsShot = () => {
-        setIsShot(true)
-    }
-
-    const handleSetHasShip = () => {
-        setHasShip(true);
-    }
-
-    useEffect(() => {
-        for (let ship in shipPlacements) {
-            if (shipPlacements[ship].coordinates.includes(coordinate)) {
-                handleSetHasShip()
-            }
-        }
-    })
-    return (
-        <div 
-        onClick={handleIsShot}
-        style={{width: (1/10) * refWidth, height: (1/10) * refHeight}}
-        className="black-border border-box flex"
-        data-coordinate={coordinate}
-        >
-            {(isShot) ?
-            <div 
-                style={{width: (1/20) * refWidth, height: (1/20) * refHeight, borderRadius: 50}}
-                className={`${hasShip ? "green" : "red"}`}
-            >
-            </div> 
-            :
-            null
-            }  
-        </div>
-    )
-}
 
 const Tiles = ({ refWidth, refHeight, shipPlacements }) => {
     const alphabetRange = "ABCDEFGHIJ".split('');
